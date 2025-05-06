ba9dd4ac31c9bb5dc50390789bdce7d5230c4c57
thanhngan1006 <125202703+thanhngan1006@users.noreply.github.com>
2025-05-06 20:52:17 +0700

    fix ui

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({ name }: Props) => {
     <div className="flex items-center gap-2 mb-8">
       <img src={logo} alt="Logo" className="w-10 h-10" />
       <div className="text-white">
-        <h1 className="font-bold">Hệ thống SV</h1>
+        <h1 className="font-bold">Hệ thống HS - GV</h1>
         <p className="text-sm opacity-80">{name}</p>
       </div>
     </div>
